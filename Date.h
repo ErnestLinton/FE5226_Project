0a9fd642b31612275c7bae5590297d4b06a1bf74
bern0 <bernadettefong@yahoo.com.sg>
2024-11-11 14:57:27 +0800

    Part 1 Rearrangement

--- Date.h
+++ Date.h
@@ -21,8 +21,8 @@ namespace minirisk {
             // convert date to serial representation
             static unsigned compute_serial(unsigned year, unsigned month, unsigned day);
 
-            // number of days elapsed from beginning of the year
-            unsigned day_of_year() const;
+            // // number of days elapsed from beginning of the year
+            // unsigned day_of_year() const;
 
             // convert serial number back to day, month, year
             static void serial_to_date(unsigned serial, unsigned& year, unsigned& month, unsigned& day);
@@ -72,41 +72,6 @@ namespace minirisk {
 
     inline double time_frac(const Date& d1, const Date& d2) {
         return static_cast<double>(d2 - d1) / 365.0;
-    }
-
-    // Compute serial number (ie. days since 1-Jan-1900)
-    unsigned Date::compute_serial(unsigned year, unsigned month, unsigned day) {
-        unsigned days = Date::days_epoch[year - Date::first_year];
-        days+= days_ytd[month -1] + ((month > 2 && is_leap_year(year)) ? 1:0 ) + (day-1);
-        return days;
-    }
-
-    // Convert serial number to a (year, month, day) date
-    void Date::serial_to_date(unsigned serial, unsigned& year, unsigned& month, unsigned& day) {
-        // Find year by iterating through days_epoch array
-        unsigned y = first_year;
-        while (serial >= days_epoch[y-first_year + 1]) ++y;
-        year = y;
-
-        // Find remaining days in year
-        unsigned days_in_year = serial - days_epoch[year - first_year];
-
-        // Find month by iterating through days_ytd
-        month = 1;
-        while(days_in_year >= days_ytd[month]) ++month;
-
-        // Adjust for leap years if date is after Feb
-        if (month > 2 && is_leap_year(year)) {
-            days_in_year-=1; // adjust for 29 feb
-            if (days_in_year == 59) { // check for 29 Feb itself
-                month = 2;
-                day = 29;
-            }
-        }
-        
-        // Calculate Day within month
-        day = days_in_year - days_ytd[month - 1] + 1;
-        
     }
 
 } // namespace minirisk
